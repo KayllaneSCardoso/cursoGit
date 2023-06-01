b8bceb626290a1551a5033ea2f91a4cd695dd123
Your Name <you@example.com>
2023-06-01 14:41:20 -0300

    leitura numeros e operador

--- revisaoAula2/main.cpp
+++ revisaoAula2/main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main(){
 	int numOne, numTwo;
-	char operator;
+	char operador;
+	cout << "Insira o primeiro numero:\n";
+	cin >> numOne;
+	cout << "Insira o segund numero:\n";
+	cin >> numTwo;
+	cout << "Insira a operacao a ser realizada: (+ 'soma', -'subtracao', * 'multiplicacao' e / 'divisao')\n";
+	cin >> operador;
 		return 0;
 }
