46ec6bfb366b36e194274a4b0c79fdd8083523f7
Your Name <you@example.com>
2023-06-06 14:48:32 -0300

    implentando subtracao

--- RevisaoAula2/main.cpp
+++ RevisaoAula2/main.cpp
@@ -6,6 +6,8 @@ int calcula(int n1, int n2, int calcOperador, int &i){
     switch(calcOperador){
         case '+': resultado = n1 + n2;
         break;
+        case '-': resultado = n1 - n2;
+        break;
     }
 }
 int main(){
