d6e777527a364348ca9843326d493e206ff7929e
Your Name <you@example.com>
2023-06-06 14:37:59 -0300

    implementando leitura de numeros

---
+++ RevisaoAula2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main(){
+	int numOne, numTwo, i, resultadoCalculo;
+	char operador;
+	cout << "Insira o primeiro numero:\n";
+	cin >> numOne;
+	cout << "Insira o segund numero:\n";
+	cin >> numTwo;
+	cout << "Insira a operacao a ser realizada: (+ 'soma', -'subtracao', * 'multiplicacao' e / 'divisao')\n";
+	cin >> operador;
+}
