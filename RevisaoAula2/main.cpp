37e33766035750afbaf7a9c54b4a65d24af24ef6
Your Name <you@example.com>
2023-06-06 14:41:52 -0300

    implentando soma

--- RevisaoAula2/main.cpp
+++ RevisaoAula2/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+int calcula(int n1, int n2, int calcOperador, int &i){
+    int resultado;
+    switch(calcOperador){
+        case '+': resultado = n1 + n2;
+        break;
+    }
+}
 int main(){
 	int numOne, numTwo, i, resultadoCalculo;
 	char operador;
